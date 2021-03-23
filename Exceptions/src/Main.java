f8e8e9663c3a9c76e66a1d3abdae1f2c38d49e91
Ignacio Lorenzo <Nachxx22@gmail.com>
2021-03-22 21:59:10 -0600

    Añadiendo el Main que no se envio en el commit de Try

---
+++ Exceptions/src/Main.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+            //Los siguentes 3 Trytest() son para probar el try, catch y finally, el try le dara la oportunidad de realizar la operación, la idea de la operación es que solamente sea
+            // de numeros positivos, en caso de que se ingrese un dato que no sea un int el programa continuara y habra hecro print del error, para continuar con los siguentes trytest
+            // el finally simplemente se utiliza para avisar de que la operación y el uso de Trytest() se acabó
+          Try.Trytest();
+          Try.Trytest();
+          Try.Trytest();
+
+}
+    }
