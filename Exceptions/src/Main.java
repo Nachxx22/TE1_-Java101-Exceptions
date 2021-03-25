0976f2bbf1c0d017d4cb3bd1a840ceed1986971d
Ignacio Lorenzo <Nachxx22@gmail.com>
2021-03-24 23:40:16 -0600

    Add throw examples

--- Exceptions/src/Main.java
+++ Exceptions/src/Main.java
@@ -7,6 +7,7 @@ public class Main {
             //Los siguentes 3 Trytest() son para probar el try, catch y finally, el try le dara la oportunidad de realizar la operación, la idea de la operación es que solamente sea
             // de numeros positivos, en caso de que se ingrese un dato que no sea un int el programa continuara y habra hecro print del error, para continuar con los siguentes trytest
             // el finally simplemente se utiliza para avisar de que la operación y el uso de Trytest() se acabó
+        //probar un trytest por lo menos ingresando un string
         //Try.Trytest();
         //Try.Trytest();
         //Try.Trytest();
