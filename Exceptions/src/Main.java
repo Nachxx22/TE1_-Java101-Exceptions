8b01bbb29aefea0b5e6876be602ab73158872a68
Ignacio Lorenzo <Nachxx22@gmail.com>
2021-03-24 18:53:39 -0600

    Add throw examples

--- Exceptions/src/Main.java
+++ Exceptions/src/Main.java
@@ -1,15 +1,20 @@
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
             //Los siguentes 3 Trytest() son para probar el try, catch y finally, el try le dara la oportunidad de realizar la operación, la idea de la operación es que solamente sea
             // de numeros positivos, en caso de que se ingrese un dato que no sea un int el programa continuara y habra hecro print del error, para continuar con los siguentes trytest
             // el finally simplemente se utiliza para avisar de que la operación y el uso de Trytest() se acabó
-          Try.Trytest();
-          Try.Trytest();
-          Try.Trytest();
-
-
+        //Try.Trytest();
+        //Try.Trytest();
+        //Try.Trytest();
+            //Los throws sirven para que podamos notificar errores de una manera más personalizada y precisa para nuestro trabajo
+            //ya que podemos elegir nosotros el mensaje del error para especificar donde se encuentra el error y ser más precisos en encontrarlos y arreglarlos
+        //Throw.test("testing");
+        //Throw.test2("testing");
+        //Throw.test3(3,0);
+        Throw.test4();
 }
     }
--- Exceptions/src/Throw.java
+++ Exceptions/src/Throw.java
@@ -1,3 +1,47 @@
-public class Throw {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.math.BigDecimal;
+import java.net.URL;
+import java.sql.*;
 
+public class Throw {
+    public static FileInputStream test(String Name) throws FileNotFoundException {
+        FileInputStream testStream = new FileInputStream(Name);
+        System.out.println("created file");
+        return testStream;
+    }
+
+    public static FileInputStream test2(String Name) throws Exception {
+        FileInputStream testStream = null;
+        try {
+            testStream = new FileInputStream(Name);
+        } catch (FileNotFoundException e) {
+            throw new Exception("El documento no existe");
+
+        } finally {
+            System.out.println("manejo de documentos finalizado");
+        }
+        return testStream;
+    }
+    public static  void test3(int a, int b) throws ArithmeticException{
+        int j;
+        try{
+            j=a/b;
+        }catch(ArithmeticException e){
+            throw new ArithmeticException("error:divisor equals 0");
+        }
+    }
+    public static void test4() throws  SQLException{
+        try {
+            String connectionUrl = "jdbc:sqlserver://;database=DB_Name;integratedSecurity=true;";
+            Connection conect = DriverManager.getConnection(connectionUrl);
+            System.out.println("Conectado.");
+        }
+        catch (SQLException e)
+        {
+            throw new SQLException("Conexion no permitida");
+        }
+    }
 }
